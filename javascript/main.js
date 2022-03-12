5a124f8877022a9401ba5376a628476aa4700c64
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-03-13 00:21:39 +0530

    code alignment

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,15 @@
-const spclChar = ["@","#","$","%","^","&","*","!"]
+const spclChar = ["@", "#", "$", "%", "^", "&", "*", "!"]
 
-function generatePass(){
+function generatePass() {
     var alphaNum = Math.random().toString(36).slice(2);
-    var spcl1 = spclChar[Math.abs(Math.ceil(Math.random()*10) - 4)]
-    var spcl2 = spclChar[Math.abs(Math.ceil(Math.random()*10) - 4)]
+    var spcl1 = spclChar[Math.abs(Math.ceil(Math.random() * 10) - 4)]
+    var spcl2 = spclChar[Math.abs(Math.ceil(Math.random() * 10) - 4)]
 
-    var password = spcl1+alphaNum+spcl2
+    var password = spcl1 + alphaNum + spcl2
     document.getElementById('pass-gen').innerHTML = password;
 }
 
-function copyToClipboard(id){
+function copyToClipboard(id) {
     var text = document.getElementById(id).innerText;
     var elem = document.createElement("textarea");
     document.body.appendChild(elem);
@@ -21,5 +21,7 @@ function copyToClipboard(id){
     //show toast message on click after copying
     var x = document.getElementById("snackbar");
     x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 3000);
 }
